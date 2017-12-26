c4512c8f2352e6209ba2e2eb0722ca1b2dfb9746
JessicaMP <jessica95.mp@gmail.com>
2017-12-25 22:33:06 -0500

    Falta habilitar el boton

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 $(function() {
   setTimeout(function() {
-    $('.color-degradiend').fadeOut();
-    $('section:last').removeClass('hide');
+    $('#splash').fadeOut();
+    $('#city').removeClass('hide');
   }, 4000);
 });
+$('.button-collapse').sideNav();
+// Page 1
+$('a.btn.white').click(function() {
+  $('#dropdown1').toggleClass('hide');
+});
+$('#dropdown1').attr({'style': 'width: 80px; position: absolute; top: 158px; left: 68px; display: block; opacity: 1'});
+// $('ul#dropdow1.dropdow-content.active').attr({'style': 'width:80px'});
