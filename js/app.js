24d431c2b928f0674dbfe38f614dbc87c2d3d39e
JessicaMP <jessica95.mp@gmail.com>
2017-12-27 17:52:54 -0500

    Codigo aleatorio funcionando

--- js/app.js
+++ js/app.js
@@ -4,10 +4,37 @@ $(function() {
     $('#city').removeClass('hide');
   }, 4000);
 });
+
 $('.button-collapse').sideNav();
-// Page 1
+
+/* __________________Page 1________________*/
 $('a.btn.white').click(function() {
   $('#dropdown1').toggleClass('hide');
 });
-$('#dropdown1').attr({'style': 'width: 80px; position: absolute; top: 158px; left: 68px; display: block; opacity: 1'});
-// $('ul#dropdow1.dropdow-content.active').attr({'style': 'width:80px'});
+
+$('#dropdown1').attr({'style': 'width: 90px; position: absolute; top: 158px; left: 108px; display: block; opacity: 1'});
+// Habilitando boton deacuerdo al contenido
+$('#input_text').keyup(function() {
+  if ($('#input_text').val().length < 10) {
+    $('#next').addClass('disabled');
+    $('#next').removeClass('btn-next');
+  } else if ($('#input_text').val().length == 10) {
+    $('#next').removeClass('grey lighten-2 disabled');
+    $('#next').addClass('btn-next');
+  }
+});
+// Codigo aleatorio LAB-000
+function rand_code(chars, lon) {
+  code = '';
+  for (x = 0; x < lon; x++) {
+    rand = Math.floor(Math.random() * chars.length);
+    code += chars.substr(rand, 1);
+  }
+  return code;
+}
+caracteres = '0123456789';
+longitud = 3;
+// Mostrando codigo aleatorio
+$('#next').click(function() {
+  alert('Tu codigo LAB-' + rand_code(caracteres, longitud));
+});
